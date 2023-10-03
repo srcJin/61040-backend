d1bdc21039420bfc3dc2e9e11f6b437b73c0808e
Jin G <gaojin@mit.edu>
2023-10-03 19:42:00 -0400

    update routes

--- server/routes.ts
+++ server/routes.ts
@@ -15,6 +15,7 @@ class Routes {
     return await User.getUserById(user);
   }
 
+  // User routes
   @Router.get("/users")
   async getUsers() {
     return await User.getUsers();
@@ -44,6 +45,8 @@ class Routes {
     return await User.delete(user);
   }
 
+  // Authentication routes
+
   @Router.post("/login")
   async logIn(session: WebSessionDoc, username: string, password: string) {
     const u = await User.authenticate(username, password);
@@ -56,6 +59,8 @@ class Routes {
     WebSession.end(session);
     return { msg: "Logged out!" };
   }
+
+  // Posts routes
 
   @Router.get("/posts")
   async getPosts(author?: string) {
@@ -89,6 +94,8 @@ class Routes {
     await Post.isAuthor(user, _id);
     return Post.delete(_id);
   }
+
+  // Friends routes
 
   @Router.get("/friends")
   async getFriends(session: WebSessionDoc) {
@@ -135,6 +142,152 @@ class Routes {
     const user = WebSession.getUser(session);
     const fromId = (await User.getUserByUsername(from))._id;
     return await Friend.rejectRequest(fromId, user);
+  }
+
+  // Profile[User] routes
+
+  @Router.get("users/:username/profile")
+  async getProfile(username: string) {
+    return null;
+  }
+
+  @Router.post("users/:username/profile")
+  async createProfile(session: WebSessionDoc, profileData: string) {
+    return null;
+  }
+
+  @Router.patch("users/:username/profile")
+  async updateProfile(session: WebSessionDoc, profileData: string) {
+    return null;
+  }
+
+  @Router.delete("users/:username/profile")
+  async deleteProfile(session: WebSessionDoc) {
+    return null;
+  }
+
+  // Map routes
+  // Map concept is different, need ask TA
+  // we are using an API to draw an app at the front end
+  // So the data the backend should provide is what the API need
+  // functions, like zoom, will only rely on frontend
+  // will need to check the API (probably leaflet) to better understand this part.
+
+  // Create a map using Leaflet API ?
+  @Router.post("/map")
+  async createMap() {
+    return null;
+  }
+
+  // Update an existing map's center point
+  @Router.patch("/map")
+  async updateMap(session: WebSessionDoc, centerPoint: string) {
+    return null;
+  }
+
+  // Delete the map (don't know if necessary )
+  @Router.delete("/map")
+  async deleteMap(session: WebSessionDoc) {
+    return null;
+  }
+
+  // Marker[Map] routes
+
+  @Router.get("/map/markers")
+  async getMarkers(mapId: ObjectId) {
+    return null;
+  }
+
+  @Router.post("/map/markers")
+  async createMarker(session: WebSessionDoc, location: string) {
+    return null;
+  }
+
+  @Router.put("/map/markers/:markerId")
+  async updateMarker(session: WebSessionDoc, mapId: ObjectId, markerId: ObjectId, updatedData: string) {
+    return null;
+  }
+
+  @Router.delete("/map/markers/:markerId")
+  async deleteMarker(session: WebSessionDoc, mapId: ObjectId, markerId: ObjectId) {
+    return null;
+  }
+
+  // Location[User] routes
+  // Retrieve a specific location by current User
+  @Router.get("users/:username/location")
+  async getSelfLocation(session: WebSessionDoc) {
+    return null;
+  }
+
+  @Router.get("users/:username/location")
+  async getUserLocation(_id: ObjectId) {
+    return null;
+  }
+
+  @Router.patch("users/:username/location")
+  async updateLocation(session: WebSessionDoc, newLocation: string) {
+    return null;
+  }
+
+  // Nearby routes：
+  @Router.get("/locations/nearby")
+  async getNearbyUsers(location: string, radius?: number) {
+    return null;
+  }
+
+  // Nearby routes：
+  @Router.get("/locations/nearby")
+  async getNearbyPOIs(location: string, radius?: number) {
+    return null;
+  }
+
+  // Reply[Post] routes
+
+  @Router.get("/posts/:_id/replies")
+  async getReplies(postId: ObjectId) {
+    return null;
+  }
+
+  @Router.post("/posts/:_id/replies")
+  async createReply(session: WebSessionDoc, content: string) {
+    return null;
+  }
+
+  // Favorite[Post] routes
+
+  @Router.post("/posts/:_id/favorite")
+  async favoritePost(session: WebSessionDoc, postId: ObjectId) {
+    return null;
+  }
+
+  @Router.delete("/posts/:_id/favorite")
+  async unfavoritePost(session: WebSessionDoc, postId: ObjectId) {
+    return null;
+  }
+
+  // Like[Post] routes
+
+  @Router.post("/posts/:_id/like")
+  async likePost(session: WebSessionDoc, postId: ObjectId) {
+    return null;
+  }
+
+  @Router.delete("/posts/:_id/like")
+  async unlikePost(session: WebSessionDoc, postId: ObjectId) {
+    return null;
+  }
+
+  // Tag routes
+
+  @Router.get("/posts/:_id/:tag")
+  async getPostsByTag(tag: string) {
+    return null;
+  }
+
+  @Router.get("/posts/replies/:tag")
+  async getRepliesByTag(tag: string) {
+    return null;
   }
 }
 
