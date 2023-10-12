a0e2a9c59097acaef2d9348b2f25f23a86e03174
Jin G <gaojin@mit.edu>
2023-10-12 07:09:07 -0400

    1st version friendship

--- server/concepts/relationship.ts
+++ server/concepts/relationship.ts
@@ -2,226 +2,173 @@ import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
 import { NotAllowedError, NotFoundError } from "./errors";
 
+export enum RelType {
+  Friend = "friend",
+  Partner = "partner",
+}
+
 export interface RelationshipDoc extends BaseDoc {
   user1: ObjectId;
   user2: ObjectId;
-  type: string;
+  relType: RelType;
 }
 
-// friend need approval
-export interface FriendRequestDoc extends BaseDoc {
+export interface RequestDoc extends BaseDoc {
   from: ObjectId;
   to: ObjectId;
-  status: "pending" | "rejected" | "accepted";
-}
-
-// partner need approval
-export interface PartnerRequestDoc extends BaseDoc {
-  from: ObjectId;
-  to: ObjectId;
+  relType: RelType;
   status: "pending" | "rejected" | "accepted";
 }
 
 export default class RelationshipConcept {
-  public readonly friends = new DocCollection<RelationshipDoc>("friends");
-  public readonly partners = new DocCollection<RelationshipDoc>("partners");
-  public readonly friendRequests = new DocCollection<FriendRequestDoc>("friendRequests");
-  public readonly partnerRequests = new DocCollection<PartnerRequestDoc>("partnerRequests");
+  private readonly relationships = new DocCollection<RelationshipDoc>("relationships");
+  private readonly requests = new DocCollection<RequestDoc>("requests");
 
-  async getFriendRequests(user: ObjectId) {
-    return await this.friendRequests.readMany({
+  async getRequests(user: ObjectId, type: RelType) {
+    return await this.requests.readMany({
       $or: [{ from: user }, { to: user }],
+      type: type,
     });
   }
 
-  async getPartnerRequests(user: ObjectId) {
-    return await this.partnerRequests.readMany({
-      $or: [{ from: user }, { to: user }],
-    });
+  async sendRequest(from: ObjectId, to: ObjectId, relType: RelType) {
+    await this.canSendRequest(from, to, relType);
+    await this.requests.createOne({ from, to, relType, status: "pending" });
+    return { msg: `Sent ${relType} request!` };
   }
 
-  async sendFriendRequest(from: ObjectId, to: ObjectId) {
-    await this.canSendRequest(from, to);
-    await this.friendRequests.createOne({ from, to, status: "pending" });
-    return { msg: "Sent friend request!" };
+  async acceptRequest(from: ObjectId, to: ObjectId, relType: RelType) {
+    await this.removePendingRequest(from, to, relType);
+    void this.requests.createOne({ from, to, relType, status: "accepted" });
+    void this.addRelationship(from, to, relType);
+    return { msg: `Accepted ${relType} request!` };
   }
 
-  async sendPartnerRequest(from: ObjectId, to: ObjectId) {
-    await this.canSendRequest(from, to);
-    await this.partnerRequests.createOne({ from, to, status: "pending" });
-    return { msg: "Sent partner request!" };
+  async rejectRequest(from: ObjectId, to: ObjectId, relType: RelType) {
+    await this.removePendingRequest(from, to, relType);
+    await this.requests.createOne({ from, to, relType, status: "rejected" });
+    return { msg: `Rejected ${relType} request!` };
   }
 
-  async acceptFriendRequest(from: ObjectId, to: ObjectId) {
-    await this.removePendingRequest(from, to);
-    // Following two can be done in parallel, thus we use `void`
-    void this.friendRequests.createOne({ from, to, status: "accepted" });
-    void this.addFriend(from, to);
-    return { msg: "Accepted friend request!" };
+  async removeRequest(from: ObjectId, to: ObjectId, relType: RelType) {
+    await this.removePendingRequest(from, to, relType);
+    return { msg: `Removed ${relType} request!` };
   }
 
-  async acceptPartnerRequest(from: ObjectId, to: ObjectId) {
-    await this.removePendingRequest(from, to);
-    // Following two can be done in parallel, thus we use `void`
-    void this.partnerRequests.createOne({ from, to, status: "accepted" });
-    void this.addPartner(from, to);
-    return { msg: "Accepted partner request!" };
-  }
-
-  async rejectRequest(from: ObjectId, to: ObjectId) {
-    await this.removePendingRequest(from, to);
-    await this.friendRequests.createOne({ from, to, status: "rejected" });
-    return { msg: "Rejected request!" };
-  }
-
-  async removeRequest(from: ObjectId, to: ObjectId) {
-    await this.removePendingRequest(from, to);
-    return { msg: "Removed request!" };
-  }
-
-  async removeFriend(user: ObjectId, friend: ObjectId) {
-    const friendship = await this.friends.popOne({
+  async removeRelationship(user: ObjectId, target: ObjectId, relType: RelType) {
+    const relationship = await this.relationships.popOne({
       $or: [
-        { user1: user, user2: friend },
-        { user1: friend, user2: user },
+        { user1: user, user2: target },
+        { user1: target, user2: user },
       ],
+      relType: RelType,
     });
-    if (friendship === null) {
-      throw new FriendNotFoundError(user, friend);
+    if (!relationship) {
+      throw new RelationshipNotFoundError(user, target, relType);
     }
-    return { msg: "Unfriended!" };
+    return { msg: `Removed ${relType}!` };
   }
 
-  // this need further debug, user must be friends before becoming partners
-  async removePartner(user: ObjectId, friend: ObjectId) {
-    const friendship = await this.partners.popOne({
-      $or: [
-        { user1: friend, user2: user },
-        { user1: user, user2: friend },
-      ],
-    });
-    if (friendship === null) {
-      throw new FriendNotFoundError(user, friend);
-    }
-    return { msg: "Unfriended!" };
-  }
-
-  async getRelationships(user: ObjectId) {
-    // Fetch friendships
-    const friendships = await this.friends.readMany({
+  async getRelationships(user: ObjectId, relType: RelType) {
+    const relationships = await this.relationships.readMany({
       $or: [{ user1: user }, { user2: user }],
+      relType: relType, // Fixed here
     });
-
-    // Fetch partnerships
-    const partnerships = await this.partners.readMany({
-      $or: [{ user1: user }, { user2: user }],
-    });
-
-    // Extract friend IDs
-    const friendIds = friendships.map((friendship) => (friendship.user1.toString() === user.toString() ? friendship.user2 : friendship.user1));
-
-    // Extract partner IDs
-    const partnerIds = partnerships.map((partnership) => (partnership.user1.toString() === user.toString() ? partnership.user2 : partnership.user1));
-
-    // Return relationships
-    return {
-      friends: friendIds,
-      partners: partnerIds,
-    };
+    return relationships.map((rel) => (rel.user1.toString() === user.toString() ? rel.user2 : rel.user1));
   }
 
-  async getFriends(user: ObjectId) {
-    const friendships = await this.friends.readMany({
-      $or: [{ user1: user }, { user2: user }],
-    });
-    // Making sure to compare ObjectId using toString()
-    return friendships.map((friendship) => (friendship.user1.toString() === user.toString() ? friendship.user2 : friendship.user1));
+  private async addRelationship(user1: ObjectId, user2: ObjectId, relType: RelType) {
+    void this.relationships.createOne({ user1, user2, relType });
   }
 
-  async getPartners(user: ObjectId) {
-    const friendships = await this.partners.readMany({
-      $or: [{ user1: user }, { user2: user }],
-    });
-    // Making sure to compare ObjectId using toString()
-    return friendships.map((friendship) => (friendship.user1.toString() === user.toString() ? friendship.user2 : friendship.user1));
-  }
-
-  private async addFriend(user1: ObjectId, user2: ObjectId) {
-    void this.friends.createOne({ user1, user2 });
-  }
-
-  private async addPartner(user1: ObjectId, user2: ObjectId) {
-    void this.partners.createOne({ user1, user2 });
-  }
-
-  private async removePendingRequest(from: ObjectId, to: ObjectId) {
-    const request = await this.friendRequests.popOne({ from, to, status: "pending" });
-    if (request === null) {
-      throw new FriendRequestNotFoundError(from, to);
+  private async removePendingRequest(from: ObjectId, to: ObjectId, relType: RelType) {
+    const request = await this.requests.popOne({ from, to, relType, status: "pending" }); // Fixed here
+    if (!request) {
+      throw new RequestNotFoundError(from, to, relType);
     }
     return request;
   }
 
-  // will change this part later for partners error handling
+  // the "parter" can only be requested when two users become a "friend"
+  // a helper function for checking if two users are friends
+  private async areFriends(user1: ObjectId, user2: ObjectId): Promise<boolean> {
+    const friendship = await this.relationships.readOne({
+      $or: [
+        { user1: user1, user2: user2 },
+        { user1: user2, user2: user1 },
+      ],
+      relType: RelType.Friend,
+    });
+    return !!friendship;
+  }
 
-  private async isNotFriends(u1: ObjectId, u2: ObjectId) {
-    const friendship = await this.friends.readOne({
+  private async canSendRequest(u1: ObjectId, u2: ObjectId, relType: RelType) {
+    const relationship = await this.relationships.readOne({
       $or: [
         { user1: u1, user2: u2 },
         { user1: u2, user2: u1 },
       ],
+      relType: relType, // Fixed here
     });
-    if (friendship !== null || u1.toString() === u2.toString()) {
-      throw new AlreadyFriendsError(u1, u2);
-    }
-  }
 
-  private async canSendRequest(u1: ObjectId, u2: ObjectId) {
-    await this.isNotFriends(u1, u2);
-    // check if there is pending request between these users
-    const request = await this.friendRequests.readOne({
+    // request for "partner" relationship is only avaliable when they are friends
+    if (relType === RelType.Partner && !(await this.areFriends(u1, u2))) {
+      throw new NotAllowedError(`Cannot request ${RelType.Partner} relationship unless both users are friends.`);
+    }
+
+    if (relationship || u1.toString() === u2.toString()) {
+      throw new AlreadyRelatedError(u1, u2, relType);
+    }
+
+    const request = await this.requests.readOne({
       from: { $in: [u1, u2] },
       to: { $in: [u1, u2] },
+      relType: relType, // Fixed here
       status: "pending",
     });
-    if (request !== null) {
-      throw new FriendRequestAlreadyExistsError(u1, u2);
+    if (request) {
+      throw new RequestAlreadyExistsError(u1, u2, relType);
     }
   }
 }
 
-export class FriendRequestNotFoundError extends NotFoundError {
+// Errors
+export class RequestNotFoundError extends NotFoundError {
   constructor(
     public readonly from: ObjectId,
     public readonly to: ObjectId,
+    public readonly relType: RelType,
   ) {
-    super("Friend request from {0} to {1} does not exist!", from, to);
+    super(`${relType} request from ${from} to ${to} does not exist!`); // fixed here
   }
 }
 
-export class FriendRequestAlreadyExistsError extends NotAllowedError {
+export class RequestAlreadyExistsError extends NotAllowedError {
   constructor(
     public readonly from: ObjectId,
     public readonly to: ObjectId,
+    public readonly relType: RelType,
   ) {
-    super("Friend request between {0} and {1} already exists!", from, to);
+    super(`${relType} request between ${from} and ${to} already exists!`); // fixed here
   }
 }
 
-export class FriendNotFoundError extends NotFoundError {
+export class RelationshipNotFoundError extends NotFoundError {
   constructor(
     public readonly user1: ObjectId,
     public readonly user2: ObjectId,
+    public readonly relType: RelType,
   ) {
-    super("Friendship between {0} and {1} does not exist!", user1, user2);
+    super(`Relationship of type ${relType} between ${user1} and ${user2} does not exist!`); // fixed here
   }
 }
 
-export class AlreadyFriendsError extends NotAllowedError {
+export class AlreadyRelatedError extends NotAllowedError {
   constructor(
     public readonly user1: ObjectId,
     public readonly user2: ObjectId,
+    public readonly relType: RelType,
   ) {
-    super("{0} and {1} are already friends!", user1, user2);
+    super(`${user1} and ${user2} are already in a ${relType} relationship!`); // fixed here
   }
 }
